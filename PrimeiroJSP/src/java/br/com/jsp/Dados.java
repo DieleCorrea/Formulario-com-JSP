f90416094f3e3a34e3e553df02baf4f5aaaece6c
Diele <dybs1520@gmail.com>
2023-08-22 16:53:31 -0300

    Create Dados.java

---
+++ PrimeiroJSP/src/java/br/com/jsp/Dados.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.jsp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Diéle
+ */
+public class Dados {
+
+    public static List<Dados> getLista() {
+        return lista;
+    }
+
+    public static void setLista(List<Dados> aLista) {
+        lista = aLista;
+    }
+    private String nome;
+    private String cidade;
+    private String endereco;
+    private String fone;
+    private int idade;
+    
+    private static List<Dados> lista = new ArrayList();
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getFone() {
+        return fone;
+    }
+
+    public void setFone(String fone) {
+        this.fone = fone;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    
+    
+}
